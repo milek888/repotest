51f8b4073754575d84f361c513b97a2f9f0c7ec1
hhh <milosz.mazurek@gmail.com>
2018-01-19 13:31:59 +0100

    I added loging active profile

--- src/main/java/springboot/Application.java
+++ src/main/java/springboot/Application.java
@@ -1,12 +1,27 @@
 package springboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 @EnableWebMvc
-public class Application {
+public class Application implements ApplicationListener<ContextRefreshedEvent> {
+
+    private static final Logger LOG = Logger.getLogger("Application.class");
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        LOG.info("activeProfile: " + activeProfile);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
