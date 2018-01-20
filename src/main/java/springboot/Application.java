f5bad691ea7b548a1fca7d2ee243f376a3f04cfa
miloszWp <milosz.mazurek@wp.pl>
2018-01-20 19:51:37 +0100

    '3333333333333'

--- src/main/java/springboot/Application.java
+++ src/main/java/springboot/Application.java
@@ -16,6 +16,7 @@ public class Application implements ApplicationListener<ContextRefreshedEvent> {
 //666666666666666
 //7777777777777777777777777
 //888888888888888888888888888888
+//999999999999999999
 
     private static final Logger LOG = Logger.getLogger("Application.class");
 
